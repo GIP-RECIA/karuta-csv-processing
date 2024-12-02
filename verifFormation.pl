bb3a764f7a04311be712aa377d4a4165b29ca16a
Pierre Legay <pierre.legay@recia.fr>
2024-12-02 11:52:46 +0100

    script de verif de code / formation pour orleans

---
+++ verifFormation.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+use strict;
+use utf8;
+use open qw( :encoding(utf8) :std );
+
+=pod
+	Synopsie : verifFormation.pl fichierCodeEtape formation.csv
+	
+	script  pour orleans pour verifier le fichierCodeEtape des code etape que l'on garde pour être sure que l'on a une ligne dans formation.csv 
+
+=cut
+my $codeFile = shift;
+my $formationFile = shift;
+
+open COD, "$codeFile" or die "$codeFile $!";
+open FORM, "$formationFile" or die "$formationFile $!";
+
+my %allFormationInList;
+
+while (<COD>) {
+	chop ;
+	next if /^\s*(#.*)?$/;
+	$allFormationInList{$_}=0;
+}
+
+print "Codes manquants :\n";
+while (<FORM>) {
+	if (/^\"(......)\"\,/) {
+		if (exists $allFormationInList{$_}) {
+			$allFormationInList{$_}=1;
+		} else {
+			print $_, "\n";
+		}
+		
+	}
+}
+
+print "\nFormations manquantes :\n";
+while (my ($k, $v) =  each %allFormationInList) {
+	unless ($v) {
+		print $k,  "\n";
+	}
+}
